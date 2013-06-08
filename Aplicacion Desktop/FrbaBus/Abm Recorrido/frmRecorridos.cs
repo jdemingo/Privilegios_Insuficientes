655956df62c1cfcd15b600e896de62e4dc43e653
mauriciojfernand <mauricioj.fernandez@gmail.com>
2013-06-08 13:00:51 -0300

    abmreco

--- Aplicacion Desktop/FrbaBus/Abm Recorrido/frmRecorridos.cs
+++ Aplicacion Desktop/FrbaBus/Abm Recorrido/frmRecorridos.cs
@@ -9,11 +9,16 @@ using System.Windows.Forms;
 
 namespace FrbaBus.Abm_Recorrido
 {
-    public partial class Form1 : Form
+    public partial class frmRecorridos : Form
     {
-        public Form1()
+        public frmRecorridos()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
