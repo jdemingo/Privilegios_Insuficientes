ee2cd0698d2a7a6873e250c11246a842bca5a52b
mauriciojfernand <mauricioj.fernandez@gmail.com>
2013-06-08 13:50:01 -0300

    JAV

--- Aplicacion Desktop/FrbaBus/FrbaUtnBus.cs
+++ Aplicacion Desktop/FrbaBus/FrbaUtnBus.cs
@@ -6,29 +6,74 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
 
 namespace FrbaBus
 {
     public partial class frmUtnBus : Form
     {
+        DataTable tablaOrigen;
+        DataTable tablaDestino;
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
         public frmUtnBus()
         {
             InitializeComponent();
+            Common.conectar();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             Form frmMicros;
             frmMicros = new FrbaBus.Abm_Micro.frmMicros();
             frmMicros.Visible = true;
         }
-
+        private void crearMenu(int rol)
+        {
+            MainMenu mainMenu = new MainMenu();
+            this.Menu = mainMenu;
+            MenuItem menuABM = new MenuItem("&ABMs");
+            MenuItem menuABMMicros = new MenuItem("&Micros");
+            MenuItem menuABMRecorridos = new MenuItem("&Recorridos");
+            mainMenu.MenuItems.Add(menuABM);
+            menuABM.MenuItems.Add(menuABMMicros);
+            menuABM.MenuItems.Add(menuABMRecorridos);
+            menuABMMicros.Click += new System.EventHandler(this.menuABMMicros_Click);
+            menuABMRecorridos.Click += new System.EventHandler(this.menuABMRecorridos_Click);
+        }
+        private void crearCombos()
+        {
+            using (Common.conn)
+            {
+                try
+                {
+                    string query = "select * from ciudades";
+                    cmd = new SqlCommand(query, Common.conn);
+                    adapter = new SqlDataAdapter(cmd);
+                    tablaOrigen = new DataTable();
+                    tablaDestino = new DataTable();
+                    adapter.Fill(tablaOrigen);
+                    adapter.Fill(tablaDestino);
+                    cmbOrigen.DisplayMember = "ciud_nombre";
+                    cmbOrigen.DataSource = tablaOrigen;
+                    cmbOrigHide.DisplayMember = "ciud_id";
+                    cmbOrigHide.DataSource = tablaOrigen;
+                    cmbDestino.DisplayMember = "ciud_nombre";
+                    cmbDestino.DataSource = tablaDestino;
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex.Message);
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
         private void frmUtnBus_Load(object sender, EventArgs e)
         {
-        }
-        private void frmUtnBus_Key(object sender, System.Windows.Forms.KeyEventArgs e)
-        {
-            if (e.Control) textBox1.Text = "prueba";
+            crearCombos();
+            crearMenu(1);
         }
         private void menuABMMicros_Click(object sender, EventArgs e)
         {
@@ -43,26 +88,33 @@ namespace FrbaBus
             frmMicros.Visible = true;
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void cmdLogin_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Cuando presionan este boton debe pedir el login y Crea el Menu");
+ 
+        }
 
-            MainMenu mainMenu = new MainMenu();
-            this.Menu = mainMenu;
-            MenuItem menuABM = new MenuItem("&ABMs");
-            MenuItem menuABMMicros = new MenuItem("&Micros");
-            MenuItem menuABMRecorridos = new MenuItem("&Recorridos");
-            mainMenu.MenuItems.Add(menuABM);
-            menuABM.MenuItems.Add(menuABMMicros);
-            menuABM.MenuItems.Add(menuABMRecorridos);
-            menuABMMicros.Click += new System.EventHandler(this.menuABMMicros_Click);
-            menuABMRecorridos.Click += new System.EventHandler(this.menuABMRecorridos_Click);
+        private static string buildConnectionURL(string user, string passwd, string server, string db) {
+            return "user id=" + user + ";password=" + passwd + ";server=" + server + ";database=" + db + "; ";
+        }
 
+        private void Conectar()
+        {
+
+            //Tomar de archivo !!
+            string user = "gd";
+            string passwd = "gd2013";
+            string server = "localhost\\SQLSERVER2008";
+            string db ="GD1C2013";
+ 
+            SqlConnection conexion = new SqlConnection(buildConnectionURL(user, passwd, server, db));
+
+            conexion.Close();
+        }
+
+        private void dateSalida_ValueChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(String.Format("{0:MM DD YYYY}", dateSalida.Text));
         }
     }
 }
---
+++ Aplicacion Desktop/FrbaBus/common.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace FrbaBus
+{
+    public class Common
+    {
+        public static SqlConnection conn;
+        private static string buildConnectionURL(string user, string passwd, string server, string db)
+        {
+            return "user id=" + user + ";password=" + passwd + ";server=" + server + ";database=" + db + "; ";
+        }
+        public static void conectar(){
+        //Tomar de archivo !!
+        string user = "gd";
+        string passwd = "gd2013";
+        string server = "localhost\\SQLSERVER2008";
+        string db = "GD1C2013";
+
+        conn = new SqlConnection(buildConnectionURL(user, passwd, server, db));
+        conn.Open();
+        }
+     }
+}
